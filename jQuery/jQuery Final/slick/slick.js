86d9115b3e5c7d8556a05ee79ec1d8225195cf1d
brianjoseph1 <bombio.brn@gmail.com>
2025-07-18 08:38:04 +0800

    modified final jquery proj

---
+++ jQuery/jQuery Final/slick/slick.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.tech-stacks').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 4,
+    infinite: true,
+    dots: true,
+    arrows: true
+})});
---
+++ jQuery/jQuery Final/typed.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    // Initialize Typed.js with options
+    var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 50, // Speed in milliseconds
+    backspeed: 150,
+    loop: true
+  });
+});
+
